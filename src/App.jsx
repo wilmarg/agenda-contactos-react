038fd09731cf81d727c24faf2c22e065bae42589
Wilmar Galvis <wagalvis24@gmail.com>
2026-09-04 19:52:44 -0500

    edit Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Contactos from './components/Contactos';
 import Footer from './components/Footer';
 
